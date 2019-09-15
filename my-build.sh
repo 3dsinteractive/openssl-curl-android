8f69cb252d8eb8c39f654a3c8160c2533e6a750a
pong3ds <chaiyapong@3dsinteractive.com>
2019-09-15 13:31:39 +0700

    Update my-build to init submodule

--- my-build.sh
+++ my-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-export ANDROID_NDK_HOME=$HOME/Library/Android/sdk/ndk-bundle
+git submodule update --init --recursive
+export ANDROID_NDK_HOME=$HOME/Library/Android/sdk/ndk/20.0.5594570
 export HOST_TAG=darwin-x86_64
 export MIN_SDK_VERSION=21
 
 export CFLAGS="-Os -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables"
 export LDFLAGS="-Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections"
-
+chmod +x ./build.sh
 ./build.sh
